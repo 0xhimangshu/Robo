d5b4a2b2640adb8f3f72bf8bc4f01720578fe067
himangshu147-git <saikiahimangshu1256@gmail.com>
2023-11-13 16:33:09 +0530

    fixed some bugs uhh

--- cogs/misc/__init__.py
+++
@@ -1,6 +0,0 @@
-from .misc import Misc
-from core import Robo   
-
-async def setup(bot: Robo):
-    await bot.add_cog(Misc(bot))
-    
--- main.py
+++
@@ -1,6 +0,0 @@
-from core import Robo
-from helpcommand import PaginatedHelpCommand
-
-if __name__=="__main__":
-    bot = Robo(help_command=PaginatedHelpCommand())
-    bot.boot()
